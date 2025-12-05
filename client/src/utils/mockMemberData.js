36ddda4914d8b408f9557d12bb479d14d0d4a658
Sarvadnya-Patil <sarvadnyapatil04@gmail.com>
2025-12-05 22:57:40 +0530

    Update mockMemberData.js

--- client/src/utils/mockMemberData.js
+++ client/src/utils/mockMemberData.js
@@ -994,7 +994,7 @@ export const mockMembers = [
     bio: 'My name is Sarvadnya Patil, and I am a first-year Computer Science and Engineering student at KLE Technological University with a strong interest in full-stack web development. I have mastered C and C++ and enjoy creating clean, user-friendly frontend designs along with efficient backend logic. I am known for strong analytical thinking, logical reasoning, and clear problem-solving ability, and I am focused on building practical projects that help me grow as a developer.',
     image: '/images/members/Sarvadnya-Patil.jpg',
     linkedin:'https://www.linkedin.com/in/sarvadnya-patil-946440393/',
-    github:'https://github.com/sarvadnyapatil04',
+    github:'https://github.com/Sarvadnya-Patil',
     instagram:'https://www.instagram.com/sarvadnyapatil_004',
   }
 ];
